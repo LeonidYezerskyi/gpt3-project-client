5fc2330abdb27acdef47386ca361ca17be830528
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2023-06-22 01:08:42 +0300

    ADDED: general styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='gpt3__navbar'>
+            <div className='gpt3__navbar-links'>
+
+            </div>
+        </div>
     )
 }
 
